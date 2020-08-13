d4306c17229c9835e93dae3f9fad9bdc56b83172
Michał Dulko <mdulko@redhat.com>
2020-08-13 16:17:46 +0200

    Test other RPM versioning
    Change-Id: Ic66e3403288401bc9645721c7cbe66613eee5a9c

--- tools/build-rpm-rhel8.sh
+++ tools/build-rpm-rhel8.sh
@@ -4,6 +4,8 @@ source_path=_output/SOURCES
 
 mkdir -p ${source_path}
 
+env
+
 # Getting version from last tag in git
 version=`git describe --tags --abbrev=0`
 version=${version#openshift-kuryr-} # remove prefix
@@ -25,5 +27,5 @@ curl http://base-openstack-4-6.ocp.svc > /etc/yum.repos.d/base-openstack-4-6.rep
 
 yum install -y python3-pbr python3-devel
 
-rpmbuild -ba -D "_version $version" -D "_release $release" -D "_topdir `pwd`/_output" openshift-kuryr-kubernetes-rhel8.spec
+rpmbuild -ba -D "_version $OS_RPM_VERSION" -D "_release $OS_RPM_RELEASE" -D "_topdir `pwd`/_output" openshift-kuryr-kubernetes-rhel8.spec
 createrepo _output/RPMS/noarch
